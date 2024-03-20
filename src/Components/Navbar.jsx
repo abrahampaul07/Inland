70d5a0b4a1d6dd2a189a3e267e90e311b1350c4f
TYT_AKHILP <akhilise.rymec@gmail.com>
2024-03-20 16:26:23 +0530

    new branch addition

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="bg-gray-800">
+    <nav className="text-gold">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <div className="flex items-center">
@@ -17,9 +17,9 @@ const Navbar = () => {
             </div>
             <div className="hidden md:block">
               <div className="ml-10 flex items-baseline space-x-4">
-                <a href="/" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Home</a>
-                <a href="/about" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">About</a>
-                <a href="/contact" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Contact</a>
+                <a href="/" className="text-gold-300 hover:bg-gray-700 hover:text-black px-3 py-2 rounded-md text-sm font-medium">Home</a>
+                <a href="/about" className="text-gold-300 hover:bg-gray-700 hover:text-black px-3 py-2 rounded-md text-sm font-medium">About</a>
+                <a href="/contact" className="text-gold-300 hover:bg-gray-700 hover:text-black px-3 py-2 rounded-md text-sm font-medium">Contact</a>
               </div>
             </div>
           </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,19 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff',
+      'purple': '#3f3cbb',
+      'midnight': '#121063',
+      'metal': '#565584',
+      'tahiti': '#3ab7bf',
+      'silver': '#ecebff',
+      'bubble-gum': '#ff77e9',
+      'bermuda': '#78dcca',
+      'gold':'#eab308'
+    },
   },
   plugins: [],
 }
