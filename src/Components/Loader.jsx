9343e317bd790c7672452585d9940d0b7ec6fc7b
TYT_AKHILP <akhilise.rymec@gmail.com>
2024-03-26 02:10:16 +0530

    back to light mode , footer addeddddd

--- src/Components/Loader.jsx
+++ src/Components/Loader.jsx
@@ -3,7 +3,7 @@ import loading from '../assets/animation.gif'
 
 const Loader = () => {
   return (
-    <div className="loader-container bg-black fixed top-0 left-0 w-full  h-full flex justify-center items-center ">
+    <div className="loader-container bg-white fixed top-0 left-0 w-full  h-full flex justify-center items-center ">
     <img src={loading} alt="Loading..." className='w-36 h-36 lg:h-64 lg:w-64'/>
   </div>
   )
